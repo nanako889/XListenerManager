ddfba38444d1e7355002dc30ba7bd081f9ac8dc9
qbw <qbaowei@qq.com>
2017-09-22 14:13:52 +0800

    1.1.1

--- xlistener/src/main/java/com/qbw/util/xlistener/ListenerManager.java
+++ xlistener/src/main/java/com/qbw/util/xlistener/ListenerManager.java
@@ -1,24 +1,30 @@
 package com.qbw.util.xlistener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 public abstract class ListenerManager<T> {
 
-    private Vector<T> mListeners = new Vector<>();
+    private List<T> mListeners = new ArrayList<>();
 
     public void addListener(T listener) {
-        if (!mListeners.contains(listener)) {
-            mListeners.add(listener);
+        synchronized (this) {
+            if (!mListeners.contains(listener)) {
+                mListeners.add(listener);
+            }
         }
     }
 
     public void removeListener(T listener) {
-        if (mListeners.contains(listener)) {
-            mListeners.remove(listener);
+        synchronized (this) {
+            if (mListeners.contains(listener)) {
+                mListeners.remove(listener);
+            }
         }
     }
 
-    public Vector<T> getListeners() {
+    public List<T> getListeners() {
         return mListeners;
     }
 }
--- xlistener/src/main/java/com/qbw/util/xlistener/XListenerManager.java
+++ xlistener/src/main/java/com/qbw/util/xlistener/XListenerManager.java
@@ -1,5 +1,10 @@
 package com.qbw.util.xlistener;
 
+/**
+ * Vector本身是线程间同步的，但是只是针对于其自身函数，而我们自己的逻辑还是要做同步处理
+ * <p>
+ * 所以采用List+同步处理，而不采用Vector
+ */
 public class XListenerManager extends ListenerManager<XListenerManager.XListener> {
 
     private static XListenerManager sInst;
@@ -18,15 +23,19 @@ public class XListenerManager extends ListenerManager<XListenerManager.XListener
     }
 
     public void notify(Object o) {
-        for (XListener listener : getListeners()) {
-            listener.onXListen(o);
+        synchronized (this) {
+            for (XListener listener : getListeners()) {
+                listener.onXListen(o);
+            }
         }
     }
 
     public void notify2(Object type, Object o) {
-        for (XListener listener : getListeners()) {
-            if (listener instanceof XListener2) {
-                ((XListener2) listener).onXListen2(type, o);
+        synchronized (this) {
+            for (XListener listener : getListeners()) {
+                if (listener instanceof XListener2) {
+                    ((XListener2) listener).onXListen2(type, o);
+                }
             }
         }
     }
