baa4f9d8014683c1741ca335a08d643468381849
qbw <qbaowei@qq.com>
2017-09-23 15:43:12 +0800

    1.1.4

--- xlistener/src/main/java/com/qbw/util/xlistener/XListenerManager.java
+++ xlistener/src/main/java/com/qbw/util/xlistener/XListenerManager.java
@@ -2,11 +2,6 @@ package com.qbw.util.xlistener;
 
 import java.util.ArrayList;
 
-/**
- * Vector本身是线程间同步的，但是只是针对于其自身函数，而我们自己的逻辑还是要做同步处理
- * <p>
- * 所以采用List+同步处理，而不采用Vector
- */
 public class XListenerManager {
 
     private static XListenerManager sInst;
@@ -46,15 +41,19 @@ public class XListenerManager {
 
     public void notify(Object o) {
         synchronized (mListeners) {
-            for (XListener listener : mListeners) {
-                listener.onXListen(o);
+            int s = mListeners.size();
+            for (int i = 0; i < s; i++) {
+                mListeners.get(i).onXListen(o);
             }
         }
     }
 
     public void notify2(Object type, Object o) {
         synchronized (mListeners) {
-            for (XListener listener : mListeners) {
+            XListener listener;
+            int s = mListeners.size();
+            for (int i = 0; i < s; i++) {
+                listener = mListeners.get(i);
                 if (listener instanceof XListener2) {
                     ((XListener2) listener).onXListen2(type, o);
                 }
